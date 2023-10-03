d59c2be72beaa7433d8a609ade80c5392ddc2b6c
hanjuuuuuu <hanjucomss@naver.com>
2023-10-04 01:04:04 +0900

    마우스 올렸을 때 설명 보이게 하기

--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -1,11 +1,33 @@
 import "../../src/index.js";
+import { useState } from "react";
 
-function Movie({ title, average, poster }) {
+function Movie({ title, average, poster, overview }) {
+  const [isOverview, setisOverview] = useState(false);
+
+  const handleOnMouse = () => {
+    setisOverview(true);
+  };
+
+  const handleOffMouse = () => {
+    setisOverview(false);
+  };
+
   return (
-    <div className="movie">
+    <div
+      className="movie"
+      onMouseEnter={handleOnMouse}
+      onMouseLeave={handleOffMouse}>
       <div className="movie-container">
         <img className="poster" src={poster} alt={title} />
 
+        {isOverview && (
+          <div className="movie-modal">
+            <div className="overview-overlay">
+              <h3 className="movie-overview">{title}</h3>
+              <h3 className="movie-overview">{overview}</h3>
+            </div>
+          </div>
+        )}
         <div className="movie-info">
           <h2 className="movie-title">{title}</h2>
           <h2 className="movie-average">{average}</h2>
